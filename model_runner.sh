fb898e9bd3f39ecea4685f7c0a55bd9f6fa1c9cb
nicolasleonri <nicolas.leonri@gmail.com>
2024-09-02 21:20:39 +0200

    fixed bash

--- model_runner.sh
+++ model_runner.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-MODELS_TOGETHER_AI=(
+MODELS=(
 	"Meta-Llama-3.1-70B-Instruct-Turbo"
 	"Qwen2-72B-Instruct"
 	"gemma-2-27b-it"
 	"Mixtral-8x22B-Instruct-v0.1"
-)
-
-MODELS_GOOGLE=(
 	"gemini-1.0-pro-002"
-)
-
-MODELS_CLAUDE=(
 	"claude-3-5-sonnet-20240620"
 )
 
@@ -33,30 +27,18 @@ function make_run_with_models() {
   
   for model in "${MODELS[@]}"; do
     # first make the single run with the programmatic player
+	echo "Make programmatic run with: $model"
     # python3 scripts/cli.py run -g wizardsapprentice -e full_programmatic2 -m "$model" programmatic programmatic
-    echo "Make programmatic run with: $model"
   
     # next: start a loop over all experiments
     for experiment in "${EXPERIMENTS[@]}"; do
-      # now make a run for each experiment with the custom player
-      # python3 scripts/cli.py run -g wizardsapprentice -e $experiment -m $model custom custom
-      echo "Make custom run with: $model for $experiment."
-
+	  	# now make a run for each experiment with the custom player
+		echo "Make custom run with: $model for $experiment."
+      	# python3 scripts/cli.py run -g wizardsapprentice -e $experiment -m $model custom custom
+      
     done
   done
 }
 
-
-# Make the runs with together ai models
-echo "cp key_togetherai.json key.json"
-make_run_with_models "${MODELS_TOGETHER_AI[@]}"
-
-# Make the runs with google models
-echo "cp key_google.json key.json"
-make_run_with_models "${MODELS_GOOGLE[@]}"
-
-# Make the runs with claude models
-echo "cp key_claude.json key.json"
-make_run_with_models "${MODELS_CLAUDE[@]}"
-
+make_run_with_models "${MODELS[@]}"
 
